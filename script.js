7c05741764ea0a42c683a4b1b1121676484a57a4
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-04-20 23:07:55 +0500

    9.0

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-// Data and Time
+// Local Storage
 
-// Data Object always Carry Both "Data and "Time"
+// In Previous todo list task when we refresh the browser we 
+// lose our current state data
 
-//  Using Data method
+// Store and retrieve data from database
 
-let currentData = new Date(21,12,2);
-console.log(currentData)
-// console.log("fdh")
+// Store and retrieve data from local storage
+
+// is an api provided by brwoser to store data inside brwoser
+// console.log(window.localStorage)
 
 
-let currentDate = new Date(0)
-console.log(currentDate)
+// set the item in local storage
+// localStorage.setItem('passion','programming')
+// localStorage.setItem('age',23)
+// console.log(localStorage)
 
+// Get the item from local storage
+
+// console.log(localStorage.getItem('passion'))
+
+// localStorage.removeItem('age')
+
+// localStorage.clear()
+
+//  ********* 9.4 *********
+// How to store complex data structure like
+// array
+
+// Ultimately in local storage data store in string formate
+
+let vehicles = [
+    {company:"Honda", model:'2008'},
+    {company:"Toyota",model:"2010"}
+]
+
+console.log( "Type of vehicles data is ",typeof vehicles)
+
+let stringOfVehicles = JSON.stringify(vehicles)
+console.log(stringOfVehicles)
+
+localStorage.setItem("vehicles", stringOfVehicles)
+console.log(localStorage)
+
+// Get stored item again in object formate
+
+let storedData = localStorage.getItem('vehicles')
+
+let objectFormat = JSON.parse(storedData)
+console.log(typeof objectFormat)
