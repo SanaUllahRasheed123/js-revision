f7315268753d4ee4a63ecf42a00a2de51ebb3688
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-04-21 16:12:18 +0500

    updated

--- script.js
+++ script.js
@@ -1,216 +1,113 @@
-//  Object Oriented Programing
+// Aynchronous JavaScript
 
-// Constructor function and "new " Operator
+//  Asyn code ********11.1
 
-//  we cannot use "arrow" function as constructor
+// console.log(1)
+// console.log(2)
 
-//  Call "constructor" fucntion using "new" keyword
+// // Asyn code
 
-// 1 new {object} created empty initially
-// 2 "this"  = {object}
-// 3 object linked to prototype
-// 4 function automatically return {object}
+// setTimeout(() => {
+//     console.log("Call back function called after 3 seconds")
+// console.log(3)
 
+// }, 3000)
+// console.log(4)
 
-// Prototype
+// setTimeout(()=>{
+// console.log(`The sum of ${5+5}  `)
+// },1000)
 
-// Each object created by "constructor " function
-// have an access to all "methods" present inside that "constructor" 
-// prototype
+//  XML Http Request ********11.2
 
-// let Car = function(color,model){
-//     // properties
-//     this.carColor= color
-//     this.carModel= model
+// Asynchronous JavaScript
 
-    // Not a good practice
-    // this.startEngine = function(){
-    //     console.log("This is start engine method of car")
-    // }
-// }
-// console.log(Car.prototype)
-// Car.prototype.startEngine = function(){
-//     console.log("This is start engine method")
-// }
+// Making HTTP Request (Example)
 
-// console.log(Car.prototype)
-//  We can also set "properties" to prototype
-// Car.prototype.company = "Honda"
-// console.log(Car.prototype)
+// let request = new XMLHttpRequest();
+// console.log(request,request.readyState)
 
-
-
-// let instanceOfCar = new Car("black",1987);
-// let instance = new Car("yellow",2830)
-// console.log(instanceOfCar,instance)
-// console.log(instanceOfCar.startEngine())
-// instanceOfCar.startEngine()
-
-// console.log(instance.__proto__)
-
-// ********* 10.4 *********ES6 Classes
-
-// ES6 Classes (They still implement prototypal inheritence
-// behing the scene)
-
-// class declaration
-
-// class Car {
-//     constructor(color,model){
-//         this.color = color;
-//         this.model= model;
+// request.addEventListener('readystatechange',()=>{
+//     // console.log(request,request.readyState)
+//     if(request.readyState ===4){
+//         console.log(request.responseText)
 //     }
-//     startEngine(){
-//         console.log("This is start engine method of Car Class")
+// })
+
+// set up the request]
+// request.open("Get","https://jsonplaceholder.typicode.com/posts")
+
+// sending the request
+
+// request.send()
+
+// Use this link for practice to get JSON data in response
+
+// value sate Description
+
+// 0 UNSENT  Client has been created . Open() not called yet.
+// 1 OPENED open() has been called
+// 2 HEADERS_RECEIVED send() has been called, and headers
+// 3 LOADING Downloading;responseText holds partial data.
+// 4 DONE The opration is complete
+
+//  Status Code ********11.3
+
+// HTTP response status codes
+
+// Informational responses (100-199)
+// Successful responses (200-399)
+// Redirectional messages (300-399)
+// Client error responses (400-499)
+// Server error responses (500-599)
+
+// let request = new XMLHttpRequest();
+
+// request.addEventListener("readystatechange",()=>{
+//     if(request.readyState===4 && request.status==200){
+//         console.log(request,request.responseText)
 //     }
-// }
+// })
 
-// let honda = new Car("Red", 2038);
-// console.log(honda.__proto__.isPrototypeOf(honda))
+// // set up the request
+// request.open("Get","https://jsonplaceholder.typicode.com/posts/")
 
-// honda.startEngine()
+// // sending the request
 
+// request.send()
 
-// Important points 
-// Classes are not hoisted
-// Classes are also first clas citizens (Pass as an argument or return)
-// Classes are executed in strict mode
+//  Call Back function********11.4
 
+let todos = (callback) => {
+  let request = new XMLHttpRequest();
 
-// setter and getters
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status == 200) {
+      //   console.log(request, request.responseText);
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      //   console.log("Data could not be fetched");
+      callback("Data Could Not Fetch", undefined);
+    }
+  });
 
-// Getters and setters allow you to define Object Accessors(Computed )
+  // set up the request
+  request.open("Get", "https://jsonplaceholder.typicode.com/posts");
 
-// We can alsoe use in class
+  // sending the request
 
-// Class declaration
+  request.send();
+};
 
-// class Car {
-//     constructor(color,model){
-//         this.color = color;
-//         this.model= model;
-//     }
-//     startEngine(){
-//         console.log("This is start engine method of Car class")
+todos((error, Data) => {
+  //   console.log("Called Function is fired");
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(Data);
+  }
+});
 
-//     }
-
-//     get __startEngine(){
-//         console.log("This is start engine method usign get")
-//     }
-//     set changeColor(color){
-//         console.log(this.color=color)
-//     }
-//     get desciption(){
-//         return `Color of Bike is ${this.color} and its model is ${this.model}`
-//     }
-    
-// }
-
-// let honda = new Car("Red",2022) // accessing as function
-// honda.startEngine()  // accessing as function
-// honda.__startEngine; // accessing as property
-
-// honda.changeColor = "blue";
-// console.log(honda);
-// console.log(honda.desciption)
-
-
-// ********* 10.6 *********Static methods
-// static method (method which is not present on 
-// constructor property but "constructor" itself)
-// class Car {
-//     constructor(color,model){
-//         this.color= color;
-//         this.model = model;
-//     }
-//     startEngine(){
-//         console.log("This is start engine method of Car class")
-//     }
-// }
-
-// static method
-// Car.breakMethod = function(){
-//     console.log("This is break method of car")
-// }
-
-// let honda = new Car("Red", 2208)
-
-// honda.startEngine()
-// Car.breakMethod()
-
-// ********* 10.7 *********Class inhertence(sub class)
-
-// class declaration
-
-// Parent Class
-
-// class Car {
-//     constructor(color,model){
-//         this.color= color;
-//         this.model = model;
-//     }
-//     startEnginer(){
-//         console.log("This is start engine method")
-//     }
-// }
-
-// child class
-
-// class Bike extends Car {
-//     // add some new properties and "method" as well
-//     constructor(color,model,enginerCapacity){
-//         super(color,model)
-//         this.enginerCapacity = enginerCapacity
-//     }
-//     bikeMehtod(){
-//         console.log("This is method of bike clas ")
-//     }
-
-// }
-
-
-// let newBike = new Bike("Black",2023,"125CC")
-// let bike2 = new Bike("orange",2432, "8r8we")
-// console.log(newBike,bike2)
-
-// let honda = new Car("red",3947);
-// console.log(honda)
-
-
-// ********* 10.9 *********chaining of methods
-
-// Chaining of methods 
-
-// classs declaration
-
-// class bankAccount {
-//     constructor(owner,pin){
-//         this.owner= owner;
-//         this.pin = pin;
-//         this.movements = [];
-//     }
-
-//     getMovements(){
-//         return this.getMovements
-//     }
-
-//     deposit(val){
-//         this.movements.push(val)
-//         return this;
-//     }
-
-//     withdraw(val){
-//         this.deposit(-val)
-//         return this;
-//     }
-// }
-
-// let account = new bankAccount("Zack",3480);
-
-// chian of methods
-// account.deposit(45).withdraw(93).deposit(1000).withdraw(25);
-// account.withdraw(65)
-
-// console.log(account.getMovements())
-// console.log(account)
+console.log(1);
+console.log(1);
+console.log(1);
