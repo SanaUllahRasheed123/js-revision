a8cd58192f34c041cc2dbe3e2abf7ef048782bf1
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2024-04-21 15:25:56 +0500

    updated

--- script.js
+++ script.js
@@ -1,51 +1,216 @@
-// Local Storage
+//  Object Oriented Programing
 
-// In Previous todo list task when we refresh the browser we 
-// lose our current state data
+// Constructor function and "new " Operator
 
-// Store and retrieve data from database
+//  we cannot use "arrow" function as constructor
 
-// Store and retrieve data from local storage
+//  Call "constructor" fucntion using "new" keyword
 
-// is an api provided by brwoser to store data inside brwoser
-// console.log(window.localStorage)
+// 1 new {object} created empty initially
+// 2 "this"  = {object}
+// 3 object linked to prototype
+// 4 function automatically return {object}
 
 
-// set the item in local storage
-// localStorage.setItem('passion','programming')
-// localStorage.setItem('age',23)
-// console.log(localStorage)
+// Prototype
 
-// Get the item from local storage
+// Each object created by "constructor " function
+// have an access to all "methods" present inside that "constructor" 
+// prototype
 
-// console.log(localStorage.getItem('passion'))
+// let Car = function(color,model){
+//     // properties
+//     this.carColor= color
+//     this.carModel= model
 
-// localStorage.removeItem('age')
+    // Not a good practice
+    // this.startEngine = function(){
+    //     console.log("This is start engine method of car")
+    // }
+// }
+// console.log(Car.prototype)
+// Car.prototype.startEngine = function(){
+//     console.log("This is start engine method")
+// }
 
-// localStorage.clear()
+// console.log(Car.prototype)
+//  We can also set "properties" to prototype
+// Car.prototype.company = "Honda"
+// console.log(Car.prototype)
 
-//  ********* 9.4 *********
-// How to store complex data structure like
-// array
 
-// Ultimately in local storage data store in string formate
 
-let vehicles = [
-    {company:"Honda", model:'2008'},
-    {company:"Toyota",model:"2010"}
-]
+// let instanceOfCar = new Car("black",1987);
+// let instance = new Car("yellow",2830)
+// console.log(instanceOfCar,instance)
+// console.log(instanceOfCar.startEngine())
+// instanceOfCar.startEngine()
 
-console.log( "Type of vehicles data is ",typeof vehicles)
+// console.log(instance.__proto__)
 
-let stringOfVehicles = JSON.stringify(vehicles)
-console.log(stringOfVehicles)
+// ********* 10.4 *********ES6 Classes
 
-localStorage.setItem("vehicles", stringOfVehicles)
-console.log(localStorage)
+// ES6 Classes (They still implement prototypal inheritence
+// behing the scene)
 
-// Get stored item again in object formate
+// class declaration
 
-let storedData = localStorage.getItem('vehicles')
+// class Car {
+//     constructor(color,model){
+//         this.color = color;
+//         this.model= model;
+//     }
+//     startEngine(){
+//         console.log("This is start engine method of Car Class")
+//     }
+// }
 
-let objectFormat = JSON.parse(storedData)
-console.log(typeof objectFormat)
+// let honda = new Car("Red", 2038);
+// console.log(honda.__proto__.isPrototypeOf(honda))
+
+// honda.startEngine()
+
+
+// Important points 
+// Classes are not hoisted
+// Classes are also first clas citizens (Pass as an argument or return)
+// Classes are executed in strict mode
+
+
+// setter and getters
+
+// Getters and setters allow you to define Object Accessors(Computed )
+
+// We can alsoe use in class
+
+// Class declaration
+
+// class Car {
+//     constructor(color,model){
+//         this.color = color;
+//         this.model= model;
+//     }
+//     startEngine(){
+//         console.log("This is start engine method of Car class")
+
+//     }
+
+//     get __startEngine(){
+//         console.log("This is start engine method usign get")
+//     }
+//     set changeColor(color){
+//         console.log(this.color=color)
+//     }
+//     get desciption(){
+//         return `Color of Bike is ${this.color} and its model is ${this.model}`
+//     }
+    
+// }
+
+// let honda = new Car("Red",2022) // accessing as function
+// honda.startEngine()  // accessing as function
+// honda.__startEngine; // accessing as property
+
+// honda.changeColor = "blue";
+// console.log(honda);
+// console.log(honda.desciption)
+
+
+// ********* 10.6 *********Static methods
+// static method (method which is not present on 
+// constructor property but "constructor" itself)
+// class Car {
+//     constructor(color,model){
+//         this.color= color;
+//         this.model = model;
+//     }
+//     startEngine(){
+//         console.log("This is start engine method of Car class")
+//     }
+// }
+
+// static method
+// Car.breakMethod = function(){
+//     console.log("This is break method of car")
+// }
+
+// let honda = new Car("Red", 2208)
+
+// honda.startEngine()
+// Car.breakMethod()
+
+// ********* 10.7 *********Class inhertence(sub class)
+
+// class declaration
+
+// Parent Class
+
+// class Car {
+//     constructor(color,model){
+//         this.color= color;
+//         this.model = model;
+//     }
+//     startEnginer(){
+//         console.log("This is start engine method")
+//     }
+// }
+
+// child class
+
+// class Bike extends Car {
+//     // add some new properties and "method" as well
+//     constructor(color,model,enginerCapacity){
+//         super(color,model)
+//         this.enginerCapacity = enginerCapacity
+//     }
+//     bikeMehtod(){
+//         console.log("This is method of bike clas ")
+//     }
+
+// }
+
+
+// let newBike = new Bike("Black",2023,"125CC")
+// let bike2 = new Bike("orange",2432, "8r8we")
+// console.log(newBike,bike2)
+
+// let honda = new Car("red",3947);
+// console.log(honda)
+
+
+// ********* 10.9 *********chaining of methods
+
+// Chaining of methods 
+
+// classs declaration
+
+// class bankAccount {
+//     constructor(owner,pin){
+//         this.owner= owner;
+//         this.pin = pin;
+//         this.movements = [];
+//     }
+
+//     getMovements(){
+//         return this.getMovements
+//     }
+
+//     deposit(val){
+//         this.movements.push(val)
+//         return this;
+//     }
+
+//     withdraw(val){
+//         this.deposit(-val)
+//         return this;
+//     }
+// }
+
+// let account = new bankAccount("Zack",3480);
+
+// chian of methods
+// account.deposit(45).withdraw(93).deposit(1000).withdraw(25);
+// account.withdraw(65)
+
+// console.log(account.getMovements())
+// console.log(account)
